ce2f625ff5932bd4bdcfbbd063d7013c07c31521
cval95 <cormacvalentine@outlook.com>
2022-02-21 18:10:58 +0000

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( <MoralisProvider
+    appId="ytXQE4NNOblY1czR0gcCS8VRxAyTU45FspZx1hpq"
+    serverUrl="https://r7ndnhrcfemr.usemoralis.com:2053/server"
+  >
+    {<Component {...pageProps} />}
+  </MoralisProvider>
+  );
 }
 
 export default MyApp
